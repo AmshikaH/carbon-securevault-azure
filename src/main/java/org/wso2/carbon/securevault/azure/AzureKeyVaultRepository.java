6b4d25a6a42adbbb9d9deef27f0864ff692a158e
AmshikaH <amshikahameed@gmail.com>
2022-01-28 14:34:39 +0530

    Update extension
    - Add support for specific versions of secrets.
    - Remove DefaultAzureCredential authentication chain and replace with a custom chain supporting only environment credentials, managed identities and the Azure CLI in order.
    - Update to authenticate once during initialization instead of during each secret retrieval.

--- src/main/java/org/wso2/carbon/securevault/azure/AzureKeyVaultRepository.java
+++ src/main/java/org/wso2/carbon/securevault/azure/AzureKeyVaultRepository.java
@@ -1,6 +1,13 @@
 package org.wso2.carbon.securevault.azure;
 
-import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.identity.AzureCliCredential;
+import com.azure.identity.AzureCliCredentialBuilder;
+import com.azure.identity.ChainedTokenCredential;
+import com.azure.identity.ChainedTokenCredentialBuilder;
+import com.azure.identity.EnvironmentCredential;
+import com.azure.identity.EnvironmentCredentialBuilder;
+import com.azure.identity.ManagedIdentityCredential;
+import com.azure.identity.ManagedIdentityCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
 import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
@@ -38,6 +45,8 @@ public class AzureKeyVaultRepository implements SecretRepository {
     private String keyVaultName;
     // String to store the client id of the user-assigned managed identity (if used) read from the configuration file
     private String managedIdentityClientId;
+    // Secret Client used to retrieve secrets from Azure Key Vault
+    private SecretClient secretClient;
 
     /**
      * Initializes the Azure Key Vault as a Secret Repository by providing configuration properties.
@@ -50,20 +59,7 @@ public class AzureKeyVaultRepository implements SecretRepository {
         if (log.isDebugEnabled()) {
             log.debug("Initializing Azure Key Vault connection.");
         }
-
-        String legacyPropertyPrefix = SECRET_REPOSITORIES + DOT + VAULT + DOT + PROPERTIES + DOT;
-        String novelPropertyPrefix = SECRET_PROVIDERS + DOT + VAULT + DOT + REPOSITORIES + DOT +
-                AZURE + PROPERTIES + DOT;
-
-        boolean novelFlag = readProviders(properties);
-        String propertyKeyVaultName = novelFlag ? (novelPropertyPrefix + KEY_VAULT_NAME) :
-                (legacyPropertyPrefix + KEY_VAULT_NAME);
-        String propertyManagedIdentityClientID = novelFlag ? (novelPropertyPrefix + MANAGED_IDENTITY_CLIENT_ID) :
-                (legacyPropertyPrefix + MANAGED_IDENTITY_CLIENT_ID);
-
-        keyVaultName = properties.getProperty(propertyKeyVaultName);
-        managedIdentityClientId = properties.getProperty(propertyManagedIdentityClientID);
-
+        readProperties(properties);
         if (StringUtils.isEmpty(managedIdentityClientId)) {
             if (log.isDebugEnabled()) {
                 log.debug("Managed identity clientId not found in configuration file. Checking environment variables.");
@@ -77,34 +73,28 @@ public class AzureKeyVaultRepository implements SecretRepository {
                 log.debug("Managed identity clientId found in configuration file. Using configured value.");
             }
         }
+        String keyVaultUri = HTTPS_COLON_DOUBLE_SLASH + keyVaultName + DOT + VAULT + DOT + AZURE + DOT + NET;
+        secretClient = new SecretClientBuilder()
+                .vaultUrl(keyVaultUri)
+                .credential(createAuthenticationChain())
+                .buildClient();
     }
 
     /**
-     * Initializes the Azure Key Vault as a Repository by providing configuration properties.
+     * Retrieves the secret from the Azure Key Vault.
      *
-     * @param alias The name of the secret being retrieved
+     * @param alias The name and version (the latter is optional) of the secret being retrieved.
      * @return The secret corresponding to the alias from the Azure Key Vault. If not found, returns alias itself.
      */
     @Override
     public String getSecret(String alias) {
-
         if (StringUtils.isEmpty(keyVaultName)) {
             if (log.isDebugEnabled()) {
                 log.debug("Azure key vault name cannot be null. Returning alias itself.");
             }
             return alias;
         }
-
-        String keyVaultUri = HTTPS_COLON_DOUBLE_SLASH + keyVaultName + DOT + VAULT + DOT + AZURE + DOT + NET;
-
-        SecretClient secretClient = new SecretClientBuilder()
-                .vaultUrl(keyVaultUri)
-                .credential(new DefaultAzureCredentialBuilder().
-                        managedIdentityClientId(managedIdentityClientId).build())
-                .buildClient();
-
-        KeyVaultSecret retrievedSecret = secretClient.getSecret(alias);
-        String secret = retrievedSecret.getValue();
+        String secret = retrieveSecretFromVault(alias);
         if (StringUtils.isEmpty(secret)) {
             if (log.isDebugEnabled()) {
                 log.debug("Could not find secret with alias '" + alias + "'. Returning alias itself.");
@@ -132,18 +122,87 @@ public class AzureKeyVaultRepository implements SecretRepository {
         return null;
     }
 
-    private boolean readProviders(Properties properties) {
+    /**
+     * Reads Carbon Secure Vault configuration properties.
+     *
+     * @param properties Configuration properties
+     */
+    private void readProperties(Properties properties) {
         String legacyProvidersString = properties.getProperty("secretRepositories", null);
+        boolean novelFlag;
         if (StringUtils.isEmpty(legacyProvidersString)) {
             if (log.isDebugEnabled()) {
                 log.debug("Legacy provider not found. Using novel configurations.");
             }
-            return true;
+            novelFlag = true;
         } else {
             if (log.isDebugEnabled()) {
                 log.debug("Legacy provider found. Using legacy configurations.");
             }
-            return false;
+            novelFlag = false;
         }
+        String legacyPropertyPrefix = SECRET_REPOSITORIES + DOT + VAULT + DOT + PROPERTIES + DOT;
+        String novelPropertyPrefix = SECRET_PROVIDERS + DOT + VAULT + DOT + REPOSITORIES + DOT +
+                AZURE + PROPERTIES + DOT;
+        String propertyKeyVaultName = novelFlag ? (novelPropertyPrefix + KEY_VAULT_NAME) :
+                (legacyPropertyPrefix + KEY_VAULT_NAME);
+        String propertyManagedIdentityClientID = novelFlag ? (novelPropertyPrefix + MANAGED_IDENTITY_CLIENT_ID) :
+                (legacyPropertyPrefix + MANAGED_IDENTITY_CLIENT_ID);
+        keyVaultName = properties.getProperty(propertyKeyVaultName);
+        managedIdentityClientId = properties.getProperty(propertyManagedIdentityClientID);
+    }
+
+    /**
+     * Retrieves the secret according to the specified version.
+     * If a secret version has not been specified, the latest version is retrieved.
+     *
+     * @param alias The name and version (the latter is optional) of the secret being retrieved.
+     * @return The secret corresponding to the alias from the Azure Key Vault.
+     */
+    private String retrieveSecretFromVault(String alias) {
+        String secretName = alias;
+        String secretVersion = "";
+        if (alias.contains("_")) {
+            int underscoreIndex = alias.indexOf("_");
+            secretName = alias.substring(0, underscoreIndex);
+            secretVersion = alias.substring(underscoreIndex + 1);
+            if (log.isDebugEnabled()) {
+                log.debug("Secret version found in alias. Retrieving the specified version of secret.");
+            }
+        } else {
+            if (log.isDebugEnabled()) {
+                log.debug("Secret version not specified in alias. Checking environment variables.");
+            }
+            secretVersion = System.getenv(alias);
+            if (StringUtils.isEmpty(secretVersion) && log.isDebugEnabled()) {
+                log.debug("Secret version not found in environment variables. Retrieving latest version of secret.");
+            }
+        }
+        KeyVaultSecret retrievedSecret = secretClient.getSecret(secretName, secretVersion);
+        String secret = retrievedSecret.getValue();
+        return secret;
+    }
+
+    /**
+     * Creates a credential chain which attempts to authenticate to the Azure Key Vault via environment credentials,
+     * a managed identity or the Azure CLI in order. If none are available, authentication fails.
+     *
+     * @return The credential chain mentioned above.
+     *
+     */
+    private ChainedTokenCredential createAuthenticationChain() {
+        EnvironmentCredential environmentCredential = new EnvironmentCredentialBuilder()
+                .build();
+        ManagedIdentityCredential managedIdentityCredential = new ManagedIdentityCredentialBuilder()
+                .clientId(managedIdentityClientId)
+                .build();
+        AzureCliCredential azureCliCredential = new AzureCliCredentialBuilder().
+                build();
+        ChainedTokenCredential credentialChain = new ChainedTokenCredentialBuilder()
+                .addFirst(environmentCredential)
+                .addLast(managedIdentityCredential)
+                .addLast(azureCliCredential)
+                .build();
+        return  credentialChain;
     }
 }
